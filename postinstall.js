38c08c9a1cb48f8a1cda0ca3cfc962c9b2dbe87c
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2021-03-19 14:49:23 +0100

    @ fix postinstall

--- postinstall.js
+++ postinstall.js
@@ -1,7 +1,10 @@
 import fs from "fs";
+import path from "path";
+import {fileURLToPath} from "url";
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const cwd = process.cwd();
 
 if (!fs.existsSync(cwd + "/gulpfile.js")) {
-    fs.copyFileSync("gulpfile.js", cwd + "/gulpfile.js")
+    fs.copyFileSync(__dirname + "/gulpfile.js", cwd + "/gulpfile.js")
 }
