51acbcc38251c66b9d203ca949d14e8f9bc519a1
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2021-12-16 16:17:31 +0100

    feat: added tailwindColors helper

--- index.js
+++ index.js
@@ -17,8 +17,25 @@ import {
 
 const defineConfig = (config) => new Core().init(config);
 
+const tailwindColors = (colors = []) => {
+    colors.forEach(name => {
+        colors[name] = ({opacityVariable, opacityValue}) => {
+            if (opacityValue !== undefined) {
+                return `rgba(var(--color-${name}), ${opacityValue})`
+            }
+            if (opacityVariable !== undefined) {
+                return `rgba(var(--color-${name}), var(${opacityVariable}, 1))`
+            }
+            return `rgb(var(--color-${name}))`
+        }
+    })
+
+    return colors
+}
+
 export {
     defineConfig,
+    tailwindColors,
     Core,
     Utils,
     Styles,
